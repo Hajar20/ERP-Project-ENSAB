8e43bed14110e57c61133c31f7fa0bb4671c2062
ABDESSAMAD <zebbaraabdessamad@gmail.com>
2023-12-13 20:03:18 +0100

    create project

---
+++ backend/src/main/java/com/erp/ensab/EnsabApplication.java
@@ -0,0 +1,13 @@
+package com.erp.ensab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsabApplication.class, args);
+	}
+
+}
