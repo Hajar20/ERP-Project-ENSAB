ad63486bf2edcb6df193cdbc10e0cb1e255b2540
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2023-12-18 15:48:01 +0100

    creating login component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginFormComponent} from "./login-form/login-form.component";
+import {PortfolioComponent} from "./portfolio/portfolio.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PortfolioComponent
+  },
+  {
+    path: 'authenticate',
+    component: LoginFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { ListOfNewsComponent } from './list-of-news/list-of-news.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ListOfNewsComponent
+    ListOfNewsComponent,
+    LoginFormComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
