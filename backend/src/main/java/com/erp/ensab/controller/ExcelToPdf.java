59e49f3480a351a1fd9b1d562e2226a88ab2159e
Imad Harir <harir.imad.2001@gmail.com>
2023-12-24 23:29:38 +0100

    add bpmn and excel file

--- backend/src/main/java/com/erp/ensab/controller/ExcelToPdf.java
+++ backend/src/main/java/com/erp/ensab/controller/ExcelToPdf.java
@@ -2,6 +2,7 @@ package com.erp.ensab.controller;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +29,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class ExcelToPdf {
