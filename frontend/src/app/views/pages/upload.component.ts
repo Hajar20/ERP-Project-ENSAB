29b0ffa8e7bd6b5af9b40becf80fe2cf6a293c7b
Imad Harir <harir.imad.2001@gmail.com>
2023-12-24 21:59:21 +0100

    generate pdf file of selected candidates to pass the oral exam

--- frontend/src/app/views/pages/upload.component.ts
+++ frontend/src/app/views/pages/upload.component.ts
@@ -1,10 +1,12 @@
 // upload.component.ts
+
 import { Component, OnInit } from '@angular/core';
 import { AppService } from "../../app.service";
 import { Router } from "@angular/router";
 import { StorageService } from "../../storage.service";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { ApiService } from 'src/app/services/api/api.service';  // Update with the actual path
+import { ApiService } from 'src/app/services/api/api.service'; // Update with the actual path
+import jsPDF from 'jspdf';
 
 @Component({
   templateUrl: 'upload.component.html',
@@ -14,27 +16,33 @@ export class UploadComponent implements OnInit {
     private router: Router,
     private app: AppService,
     private storageService: StorageService,
-    private apiService: ApiService // Add this line
+    private apiService: ApiService
   ) {}
 
   alert: boolean = false;
   selectedFile: boolean = true;
+  examData:any = [];
   myForm = new FormGroup({
     file: new FormControl('', Validators.required),
+    threshold: new FormControl('', Validators.required),
   });
 
   onSubmit() {
     const fileControl = this.myForm.get('file');
-  
-    if (fileControl && fileControl.value) {
+    const thresholdControl = this.myForm.get('threshold');
+
+    if (fileControl && fileControl.value && thresholdControl && thresholdControl.value) {
       const formData = new FormData();
       formData.append('file', fileControl.value as unknown as Blob);
-  
+      formData.append('value', thresholdControl.value);
+
       this.apiService.processExcel(formData).subscribe(
         (response: any) => {
           // Handle the API response here
           console.log(response);
+          this.examData = response;
           this.alert = true; // Show success alert
+          this.generatePDF();
         },
         (error: any) => {
           console.error(error);
@@ -43,22 +51,68 @@ export class UploadComponent implements OnInit {
       );
     } else {
       this.selectedFile = false;
-      console.log("Filename : ", fileControl?.value);
+      console.log("Filename or threshold is missing.");
     }
   }
-  
 
   getFileDetails(event: any) {
     const selectedFile = event.target.files[0];
     this.myForm.patchValue({ file: selectedFile }); // Update form control value
     this.selectedFile = true;
-    //console.log('Filename : ', this.myForm.value.file);
   }
 
   closeAlert() {
     this.selectedFile = false;
     this.alert = false;
   }
+  generatePDF() {
+    const doc = new jsPDF(); // Use 'jsPDF' directly
+  
+    // Logo on the right
+    const imgData = '../assets/images/logo1.png';
+    const logoWidth = 20;
+    const logoHeight = 10;
+    const logoX = doc.internal.pageSize.width - logoWidth - 20; // Adjust the margin as needed
+    const logoY = 10;
+    doc.addImage(imgData, 'JPEG', logoX, logoY, logoWidth, logoHeight);
+  
+    doc.text('Selected candidates to pass the oral exam', 20, 15);
+    doc.setFontSize(12);
+    doc.text(new Date().toLocaleDateString(), 20, 24);
+  
+    // Assuming the jsPDF version you are using may not have the 'getNumberOfPages' method,
+    // you can calculate the number of pages by checking the length of the 'pages' array.
+    const pageCount = doc.internal.pages.length; // Use 'pages.length' to get the count of pages
+    for (let i = 1; i < pageCount; i++) {
+      doc.setPage(i);
+      doc.text(`Page ${i} of ${pageCount}`, doc.internal.pageSize.getWidth() / 2, doc.internal.pageSize.getHeight() - 10, {
+        align: 'center'
+      });
+    }
+  
+    if (this.examData && this.examData.length > 0) {
+      const candidateData = this.examData.map((candidate: any) => {
+        return [candidate["CNE"], candidate["Nom"], candidate["Prénom"], candidate["Score"]];
+      });
+  
+      const head = ['CNE', 'Nom', 'Prénom', 'Score'];
+  
+      const startYMargin = 30;
+      doc.autoTable({
+        head: [head],
+        body: candidateData,
+        startY: startYMargin,
+        didDrawCell: (data: any) => {},
+      });
+    } else {
+      // No data available
+      doc.text('No data available', 20, 40);
+    }
+  
+    // Save the PDF
+    doc.save('candidatesPassedToOral.pdf');
+  }
+  
 
   user: any = null;
 
